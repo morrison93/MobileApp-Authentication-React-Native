9de9e61c38cb6462054894764aee08a1b5e738a0
Rodrigo <rodrigo.rocha582@gmail.com>
2020-04-23 22:47:54 +0100

    added decision tree

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 // TextInput behaves just like a image so we have to fix a width and height in style
 
 
 class LoginForm extends Component {
-	state = { email: '', password: '' };
+	state = { email: '', password: '', error: '' };
+
+	onButtonPress () {
+		const { email, password } = this.state;
+
+		firebase.auth().signInWithEmailAndPassword(email, password);
+			.catch(() =>{
+				firebase.auth().createUserWithEmailAndPassWord(email, password)
+					.catch(() => {
+						this.setState({ error: 'Authentication Failed.' });
+					});
+			});
+	}
+
 
 	render () {
 		return (
@@ -29,8 +44,12 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
+
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Log in
 					</Button>
 				</CardSection>
@@ -39,5 +58,12 @@ class LoginForm extends Component {
 	}
 }
 
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
 
 export default LoginForm;
